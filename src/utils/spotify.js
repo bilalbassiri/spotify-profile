85d09a7556786738dd5ad9dc171217f122908ceb
Bilal BASSIRI <bassiri.bilal@gmail.com>
2021-03-13 21:58:13 +0100

    Simple Modifying

--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -2,7 +2,7 @@
 
 const authEndpoint = 'https://accounts.spotify.com/authorize',
     scopes = 'user-read-recently-played user-top-read user-follow-read user-library-read playlist-read-collaborative playlist-read-private user-read-private',
-    redirectUri = 'http://localhost:3000/';
+    redirectUri = 'https://spotify-profile-app.web.app/';
 
 const loginUrl = `${authEndpoint}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&response_type=token&show_dialog=false`;
 const getTokenFromUrl = () => {
